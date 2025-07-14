2f0f5eceb46c1421c225ad6273db025fd7e1ad0e
pawanacharya73 <pawanacharya832@gmail.com>
2025-07-14 18:50:48 +0545

    grid lines on camerapreview

--- app/src/main/java/com/softwarica/sondr/view/TakeSnapshotActivity.kt
+++ app/src/main/java/com/softwarica/sondr/view/TakeSnapshotActivity.kt
@@ -15,12 +15,14 @@ import androidx.compose.ui.platform.LocalContext
 import android.app.Activity
 import android.content.Intent
 import android.widget.Toast
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.border
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -34,6 +36,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
@@ -136,10 +139,26 @@ fun TakeSnapshotBody(
                     .background(Color(0xff121212)),
                 contentAlignment = Alignment.Center
             ) {
-                CameraPreview(
-                    isFrontCamera = isFrontCamera.value,
-                    aspectRatio = aspectRatio.value
-                )  // This is where the camera preview will be displayed
+                // Inner box with fixed aspect ratio for the preview and grid
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(
+                            when (aspectRatio.value) {
+                                "3:4" -> 3f / 4f
+                                "9:16" -> 9f / 16f
+                                else -> 3f / 4f
+                            }
+                        )
+                ) {
+                    CameraPreview(
+                        isFrontCamera = isFrontCamera.value,
+                        aspectRatio = aspectRatio.value
+                    ) // this is where the camera preview is shown
+
+                    // Grid overlay
+                    GridOverlay()
+                }
             }
 
             // Bottom bar with 3 buttons
@@ -221,6 +240,47 @@ fun TakeSnapshotBody(
     }
 }
 
+@Composable
+fun GridOverlay() {
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        val width = size.width
+        val height = size.height
+
+        val thirdWidth = width / 3
+        val thirdHeight = height / 3
+
+        val lineColor = Color.White.copy(alpha = 0.3f)
+        val strokeWidth = 1.dp.toPx()
+
+        // Vertical lines
+        drawLine(
+            color = lineColor,
+            start = Offset(thirdWidth, 0f),
+            end = Offset(thirdWidth, height),
+            strokeWidth = strokeWidth
+        )
+        drawLine(
+            color = lineColor,
+            start = Offset(2 * thirdWidth, 0f),
+            end = Offset(2 * thirdWidth, height),
+            strokeWidth = strokeWidth
+        )
+
+        // Horizontal lines
+        drawLine(
+            color = lineColor,
+            start = Offset(0f, thirdHeight),
+            end = Offset(width, thirdHeight),
+            strokeWidth = strokeWidth
+        )
+        drawLine(
+            color = lineColor,
+            start = Offset(0f, 2 * thirdHeight),
+            end = Offset(width, 2 * thirdHeight),
+            strokeWidth = strokeWidth
+        )
+    }
+}
 
 
 @Preview(showBackground = true)
