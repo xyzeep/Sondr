d3c6404de33720f9f520431449a46ee7b7da1574
pawanacharya73 <pawanacharya832@gmail.com>
2025-07-17 18:52:11 +0545

    fixed ugly white screen while switching activities

--- app/src/main/java/com/softwarica/sondr/view/SplashActivity.kt
+++ app/src/main/java/com/softwarica/sondr/view/SplashActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -61,7 +62,7 @@ fun SplashScreen(onTimeout: () -> Unit) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Black),
+            .background(colorResource(id = R.color.background)),
         contentAlignment = Alignment.Center
     ) {
         Image(
