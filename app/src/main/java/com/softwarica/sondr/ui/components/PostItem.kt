ccec4fd71abd07e871e6e8a2b7c3a0daab5c1789
pawanacharya73 <pawanacharya832@gmail.com>
2025-07-20 14:50:52 +0545

    double tap to like

--- app/src/main/java/com/softwarica/sondr/ui/components/PostItem.kt
+++ app/src/main/java/com/softwarica/sondr/ui/components/PostItem.kt
@@ -14,12 +14,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import coil.compose.rememberAsyncImagePainter
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -80,10 +82,22 @@ fun PostItem(
                         .fillMaxWidth()
                         .height(300.dp)
                         .clip(RoundedCornerShape(8.dp))
-                        .combinedClickable(
-                            onClick = { if (post.nsfw) isBlurred = !isBlurred },
-                            onLongClick = { onRequestFullscreen(post.mediaRes.toString()) }
-                        )
+                        .pointerInput(Unit) {
+                            detectTapGestures(
+                                onDoubleTap = {
+                                    isLiked = !isLiked
+                                    likeCount += if (isLiked) 1 else -1
+                                    onLikeToggle(post, isLiked)
+                                },
+                                onLongPress = {
+                                    onRequestFullscreen(post.mediaRes.toString())
+                                },
+                                onTap = {
+                                    if (post.nsfw) isBlurred = !isBlurred
+                                }
+                            )
+                        }
+
                 ) {
                     Image(
                         painter = rememberAsyncImagePainter(model = post.mediaRes),
@@ -153,9 +167,7 @@ fun PostItem(
                     contentDescription = "like",
                     modifier = Modifier
                         .width(32.dp)
-                        .clickable {
-                        // TODO
-                    },
+                        ,
                     tint = (if (isLiked) Color.Red else Color.White)
                 )
 
